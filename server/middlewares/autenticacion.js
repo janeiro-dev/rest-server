6c905c326924dc512cba9adc1ec14471ef8c6b3d
Janeiro <psiqui99@gmail.com>
2020-11-13 11:09:35 -0400

    Final rest-server

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -47,8 +47,30 @@ let verificaAdmin = (req, res, next) => {
 
 }
 
+// =======================
+//verificar token img
+// =======================
+
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: 'token no valido'
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+
+    });
+}
+
 module.exports = {
     verificaToken,
-    verificaAdmin
+    verificaAdmin,
+    verificaTokenImg
 
 }
